4755713f5a6a9016af4f2e4c34d3d35ec3913294
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2018-01-13 15:49:34 +0100

    Use native timezone in query

--- reminders/views.py
+++ reminders/views.py
@@ -4,13 +4,14 @@ from django.db.models import Q
 from django.db.models import F
 
 from .models import Donor
-from datetime import datetime,timedelta
+from django.utils import timezone
+from datetime import timedelta
 
 def index(request):
-    not_recent_donation_taken = Q(donation__done_at__lte=datetime.now() - timedelta(days=30))
+    not_recent_donation_taken = Q(donation__done_at__lte=timezone.now() - timedelta(days=30))
     donation_never_taken = Q(donation__isnull=True)
 
-    not_recent_reminder_sent = Q(reminder__sent_at__lte=datetime.now() - timedelta(days=7))
+    not_recent_reminder_sent = Q(reminder__sent_at__lte=timezone.now() - timedelta(days=7))
     reminder_never_sent = Q(reminder__isnull=True)
 
     oldest_donations_list = Donor.objects.filter(
