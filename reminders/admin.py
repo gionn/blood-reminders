5ef6aeb2d657a939dacc879a3126a4ef3e736402
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2018-01-14 22:30:52 +0100

    Complex query may work

--- reminders/admin.py
+++ reminders/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
-from django.db.models import Q
-from django.db.models import F
+from django.db.models import Q,F,Max
 from django.utils import timezone
 from datetime import timedelta
 
 from .models import Donor
 from .models import Donation
 from .models import Reminder
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 class NeedsReminderSentFilter(admin.SimpleListFilter):
     title = _('reminder needed')
@@ -17,7 +20,7 @@ class NeedsReminderSentFilter(admin.SimpleListFilter):
     def lookups(self, request, model_admin):
         return (
             ('true', _('Reminder to be sent')),
-            ('false', _('Already reminded')),
+            ('false', _('Reminder not needed')),
         )
 
     def queryset(self, request, queryset):
@@ -25,31 +28,26 @@ class NeedsReminderSentFilter(admin.SimpleListFilter):
         female = Q(gender='F')
         blood_donation = Q(last_donation_type='S')
         plasma_donation = Q(last_donation_type='P')
-        not_recent_donation_taken_male = Q(donation__done_at__lte=timezone.now() - timedelta(days=90))
-        not_recent_donation_taken_female = Q(donation__done_at__lte=timezone.now() - timedelta(days=180))
-        not_recent_plasma_donation_taken = Q(donation__done_at__lte=timezone.now() - timedelta(days=30))
-        donation_never_taken = Q(donation__isnull=True)
+        not_recent_donation_taken_male = Q(last_donation__lte=timezone.now() - timedelta(days=90))
+        not_recent_donation_taken_female = Q(last_donation__lte=timezone.now() - timedelta(days=180))
+        not_recent_plasma_donation_taken = Q(last_donation__lte=timezone.now() - timedelta(days=30))
 
+        donation_never_taken = Q(donation__isnull=True)
         not_recent_reminder_sent = Q(reminder__sent_at__lte=timezone.now() - timedelta(days=7))
         reminder_never_sent = Q(reminder__isnull=True)
 
+        restriction = ( 
+            ( blood_donation & male & not_recent_donation_taken_male ) |
+            ( blood_donation & female & not_recent_donation_taken_female ) |
+            ( plasma_donation & not_recent_plasma_donation_taken )
+        ) & ( not_recent_reminder_sent | reminder_never_sent ) | donation_never_taken
+
         if self.value() == 'true':
-            return queryset.filter(
-                blood_donation & male & not_recent_donation_taken_male | donation_never_taken,
-                not_recent_reminder_sent | reminder_never_sent
-            ).union(
-                Donor.objects.filter(
-                    blood_donation & female & not_recent_donation_taken_female | donation_never_taken,
-                    not_recent_reminder_sent | reminder_never_sent
-                )
-            ).union(
-                Donor.objects.filter(
-                    plasma_donation & not_recent_plasma_donation_taken | donation_never_taken,
-                    not_recent_reminder_sent | reminder_never_sent
-                )
-            ).distinct()
+            queryset = queryset.annotate(last_donation=Max('donation__done_at')).filter(restriction).distinct()
+            logger.warn(queryset.query)
+            return queryset
         if self.value() == 'false':
-            return queryset.exclude()
+            return queryset.annotate(last_donation=Max('donation__done_at')).exclude(restriction).distinct()
 
 
 
