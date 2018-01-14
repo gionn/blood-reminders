634a7c519d98e6b3fa5f92beb3eba86f2fd9e3f5
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2018-01-14 19:26:50 +0100

    First query implementation based on unions

--- reminders/admin.py
+++ reminders/admin.py
@@ -21,7 +21,13 @@ class NeedsReminderSentFilter(admin.SimpleListFilter):
         )
 
     def queryset(self, request, queryset):
-        not_recent_donation_taken = Q(donation__done_at__lte=timezone.now() - timedelta(days=30))
+        male = Q(gender='M')
+        female = Q(gender='F')
+        blood_donation = Q(last_donation_type='S')
+        plasma_donation = Q(last_donation_type='P')
+        not_recent_donation_taken_male = Q(donation__done_at__lte=timezone.now() - timedelta(days=90))
+        not_recent_donation_taken_female = Q(donation__done_at__lte=timezone.now() - timedelta(days=180))
+        not_recent_plasma_donation_taken = Q(donation__done_at__lte=timezone.now() - timedelta(days=30))
         donation_never_taken = Q(donation__isnull=True)
 
         not_recent_reminder_sent = Q(reminder__sent_at__lte=timezone.now() - timedelta(days=7))
@@ -29,14 +35,22 @@ class NeedsReminderSentFilter(admin.SimpleListFilter):
 
         if self.value() == 'true':
             return queryset.filter(
-                not_recent_donation_taken | donation_never_taken,
+                blood_donation & male & not_recent_donation_taken_male | donation_never_taken,
                 not_recent_reminder_sent | reminder_never_sent
+            ).union(
+                Donor.objects.filter(
+                    blood_donation & female & not_recent_donation_taken_female | donation_never_taken,
+                    not_recent_reminder_sent | reminder_never_sent
+                )
+            ).union(
+                Donor.objects.filter(
+                    plasma_donation & not_recent_plasma_donation_taken | donation_never_taken,
+                    not_recent_reminder_sent | reminder_never_sent
+                )
             ).distinct()
         if self.value() == 'false':
-            return queryset.exclude(
-                not_recent_donation_taken | donation_never_taken,
-                not_recent_reminder_sent | reminder_never_sent
-            ).distinct()
+            return queryset.exclude()
+
 
 
 class DonorAdmin(admin.ModelAdmin):
