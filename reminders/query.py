3059a8d63fc6df6705c2d5791e51b90076f96577
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2018-01-21 21:24:18 +0100

    Skip donors without active donations in the last 2 years

--- reminders/query.py
+++ reminders/query.py
@@ -11,6 +11,7 @@ class DonorQuerySet():
         not_recent_donation_taken_male = Q(last_donation__lte=timezone.now() - timedelta(days=90))
         not_recent_donation_taken_female = Q(last_donation__lte=timezone.now() - timedelta(days=180))
         not_recent_plasma_donation_taken = Q(last_donation__lte=timezone.now() - timedelta(days=30))
+        has_recent_donations = Q(last_donation__gte=timezone.now() - timedelta(days=720))
 
         donation_never_taken = Q(donation__isnull=True)
         not_recent_reminder_sent = Q(reminder__sent_at__lte=timezone.now() - timedelta(days=7))
@@ -20,7 +21,7 @@ class DonorQuerySet():
             ( blood_donation & male & not_recent_donation_taken_male ) |
             ( blood_donation & female & not_recent_donation_taken_female ) |
             ( plasma_donation & not_recent_plasma_donation_taken )
-        ) & ( not_recent_reminder_sent | reminder_never_sent ) | donation_never_taken
+        ) & has_recent_donations & ( not_recent_reminder_sent | reminder_never_sent ) | donation_never_taken
 
     def get_donors_with_reminders(self, queryset):
         restriction = self.get_base_queryset()
