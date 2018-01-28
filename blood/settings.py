6fc9ec8f1236cf9eec7cb2360e9a0f383763a6e2
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2018-01-28 11:50:54 +0100

    Fix bad request with debug false

--- blood/settings.py
+++ blood/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'development_not_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
