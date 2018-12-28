4e81ff9e4db7dcafb5df7374f8b6b4fb721a8b4f
Giovanni Toraldo <me@gionn.net>
2018-12-28 10:11:33 +0100

    Fix relative time in charts, adjust graph to 10 yrs

--- charts/views.py
+++ charts/views.py
@@ -12,12 +12,14 @@ class ChartsView(View):
         last_update = Donation.objects.order_by('-done_at')[0].done_at
 
         donations_data = Donation.objects.filter(
-            done_at__gt=datetime.date(2018, 1, 1)
+                done_at__gt=datetime.datetime.now() - datetime.timedelta(weeks=52)
             ).annotate(
                 month=TruncMonth('done_at')
             ).values('month').annotate(count=Count('id')).values_list('month','count').order_by('month')
 
-        donations_yearly_data = Donation.objects.annotate(
+        donations_yearly_data = Donation.objects.filter(
+                done_at__gt=datetime.datetime.now() - datetime.timedelta(weeks=52*10)
+            ).annotate(
                 year=TruncYear('done_at')
             ).values('year').annotate(count=Count('id')).values_list('year','count').order_by('year')
 
