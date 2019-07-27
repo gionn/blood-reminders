4114cda597dead58d804c901187e5ab524dd122c
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2019-07-27 17:42:10 +0200

    Remove unused prometheus client dependency (#11)
    * Increase test wait timeout
    
    * Remove unused metrics app

--- blood/urls.py
+++ blood/urls.py
@@ -18,7 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('reminders/', include('reminders.urls')),
-    path('metrics/', include('metrics.urls')),
     path('charts/', include('charts.urls')),
     path('admin/', admin.site.urls),
 ]
--- metrics/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-from metrics.views import MetricsView
-
-urlpatterns = [
-    path('', MetricsView.as_view()),
-]
