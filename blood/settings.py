d1f6ea52716a0bf50106fe2e0dce472f36265886
Giovanni Toraldo <giovanni.toraldo@eng.it>
2020-03-28 14:39:40 +0100

    Move charts template to the proper app (#32)

--- blood/settings.py
+++ blood/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'reminders.apps.RemindersConfig',
+    'charts.apps.ChartsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
